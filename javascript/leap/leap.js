73641c7bfe508d379696975e67c5f942be096828
gitsudhir <31241120+gitsudhir@users.noreply.github.com>
2019-06-24 00:32:28 +0530

    using Date object

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,6 +1,1 @@
-export function isLeap(year) {
-    if (year & 1 == 1) return false;
-    if (year % 4 == 0 && year % 100 !== 0) return true;
-    if (year % 100 == 0 && year % 400 == 0) return true;
-    return false;
-}
+export const isLeap = (year)=> new Date(`February 29 ,${year}`).getMonth() == 1
